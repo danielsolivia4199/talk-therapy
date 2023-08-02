91b904fec9ab8ba2943c19281340eff053e4b9a4
Olivia Daniels <danielsolivia4199@gmail.com>
2023-08-01 19:42:05 -0500

    test profile

--- components/User.js
+++ components/User.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
-import Image from 'next/image';
 import { useAuth } from '../utils/context/authContext';
 import { signOut } from '../utils/auth';
 
@@ -9,7 +8,7 @@ export default function ShowUser() {
   const { user } = useAuth();
   return (
     <div className="profilepage">
-      <Image src={user.profile_image_url} alt="user" width="100px" height="100px" />
+      <img src={user.profile_image_url} alt="user" width="100px" height="100px" />
       <h1>{user.first_name} {user.last_name}</h1>
       <h3 className="bio">{user.bio}</h3>
       <h2>Email: {user.email}</h2>
@@ -21,9 +20,12 @@ export default function ShowUser() {
 
 ShowUser.propTypes = {
   userObj: PropTypes.shape({
-    displayName: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    bio: PropTypes.string,
+    profile_image_url: PropTypes.string,
     email: PropTypes.string,
-    lastLogIn: PropTypes.string,
+    created_on: PropTypes.string,
     firebaseKey: PropTypes.string,
   }).isRequired,
 };
